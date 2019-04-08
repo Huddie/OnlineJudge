b71a4a8e08fc1953be899b8e67f1cb36dcdc73cf
Ehud Adler <easports96@gmail.com>
2019-04-07 21:18:07 -0400

    Fixed dir slash

--- upload.php
+++ upload.php
@@ -1,7 +1,7 @@
 <?php
     $target_dir = "uploads/";
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    echo  dirname(__DIR__).$target_file;
+    echo  dirname(__DIR__).'/'.$target_file;
     $uploadOk = 1;
     $codeFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
